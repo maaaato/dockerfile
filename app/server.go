8ec4a67817669cf07863136def222ecc652c1147
maaaato <igosso.mato@gmail.com>
2015-05-09 23:50:51 +0900

    goji,aws-sdk-goを使ったサンプル

--- app/server.go
+++ app/server.go
@@ -2,16 +2,29 @@ package main
 
 
 import (
+	"os"
 	"fmt"
 	"net/http"
+
+	"github.com/zenazn/goji"
+    "github.com/zenazn/goji/web"
+	
+	"github.com/awslabs/aws-sdk-go/aws"
+	"github.com/awslabs/aws-sdk-go/gen/ec2"	
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello, world")
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+
+	access_key := os.Getenv("MAAAATO_AWS_ACCESS_KEY_ID")
+	secret_key := os.Getenv("MAAAATO_AWS_SECRET_ACCESS_KEY")
 	
+	creds := aws.Creds(access_key, secret_key, "")
+	cli := ec2.New(creds, "ap-northeast-1", nil)
+	resp, _ := cli.DescribeInstances(nil)
+	fmt.Fprintf(w, *resp.Reservations[0].Instances[0].VPCID)
 }
 
-func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+func main() {
+	goji.Get("/hello/:name", hello)
+	goji.Serve()
 }
